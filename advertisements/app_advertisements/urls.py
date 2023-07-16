c5e60ec8fee4fe4d232ef418fda25ce7c7194276
Nisaygax <arseniyburachok@gmail.com>
2023-07-16 20:03:56 +0300

    ver 1.5

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -3,5 +3,9 @@ from .views import index, top_sellers
 
 urlpatterns = [
     path('', index, name = 'main-page'),
-    path('top-sellers/', top_sellers, name = 'top-sellers')
+    path('top-sellers/', top_sellers, name = 'top-sellers'),
+    path('advertisement-post/', top_sellers, name = 'advertisement-post'),
+    path('register/', top_sellers, name = 'register'),
+    path('login/', top_sellers, name = 'login'),
+    path('profile/', top_sellers, name = 'profile'),
 ]
