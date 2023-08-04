fa2741c99c04f47e34856e5e8eee7b7c351e641a
Nisaygax <arseniyburachok@gmail.com>
2023-08-04 21:16:34 +0300

    ver 1.7

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Advertisement
+
+class Advertisement_Admin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'created_date', 'updated_date', 'price', 'auction']
+    list_filter = ['auction', 'created_at']
+    
+    actions = ['make_auction_false', 'make_auction_true']
+
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_false(self, request, queryset):
+        queryset.update(auction = False)
+
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_true(self, request, queryset):
+        queryset.update(auction = True)
+
+    fieldsets = (
+        ('Общее', {'fields' : ('title', 'description')}),
+        ('Финансы', {'fields' : ('price', 'auction'), 'classes': ['collapse']})
+    )
+
+
+admin.site.register(Advertisement, Advertisement_Admin)
--- advertisements/app_advertisements/apps.py
+++ advertisements/app_advertisements/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class AppAdvertisementsConfig(AppConfig):
+    verbose_name = 'Объявления'
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_advertisements'
